ed7aa0852f36449a7de2f9972832c5e1f54d3307
Aymeric Bernard <aymeric.bernard@illuin.tech>
2025-05-31 14:11:03 +0200

    fix: fix description

--- src/app/spells/spell-choice-form/spell-choice-form.component.ts
+++ src/app/spells/spell-choice-form/spell-choice-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, DestroyRef, forwardRef, inject, OnInit, output} from '@angular/core';
+import {Component, DestroyRef, forwardRef, inject, OnInit} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {
   AbstractControl,
